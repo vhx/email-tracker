39e3e603d3721bd3424e4f16bcf27c0386a69f46
Charlie Tran <hi@charlietran.com>
2014-08-22 18:25:32 -0400

    basic output

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'libraries'
 require 'uri'
 
+
 module SendgridTracker
   class App < Sinatra::Base
+    use Rack::Logger
     set :app_file, __FILE__
     set :uri, ENV["METRICS_API_URL"] || "metrics-api.librato.com"
 
@@ -17,37 +19,10 @@ module SendgridTracker
     end
 
     post '/track' do
-      unless params[:email] && params[:token] && params[:name]
-        bad_request!
-      end
-
-      conn = Faraday.new(:url => "https://#{settings.uri}") do |builder|
-        builder.request  :json
-        builder.adapter  Faraday.default_adapter
-      end
-
-      conn.basic_auth params[:email], params[:token]
-
-      body = {
-        :title => "#{params[:user]} deployed #{params[:app]}",
-        :description => params[:git_log],
-        :links => [{ :label => "App URL", :rel => 'heroku',
-                     :href => params[:url]}]
-      }
-
-      if params[:github_repo]
-        body[:links] << {
-          :label => "Github Commit",
-          :rel => 'github',
-          :href => "#{params[:github_repo]}/commit/#{params[:head_long]}"
-        }
-      end
-
-      resp = conn.post "/v1/annotations/#{params[:name]}", body do |req|
-      end
+      puts params.inspect
 
       content_type 'application/json'
-      halt(resp.status, resp.body)
+      halt(200, params.inspect)
     end
 
   end
